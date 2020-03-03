dd7e807cc6fe623b0ba98ab8ce9fde428834ce8f
Hiten Pratap Singh <hitenpratap99@gmail.com>
2020-03-03 18:54:06 +0530

    Removed test statements

--- jinja2-filters/util/template_filters.py
+++ jinja2-filters/util/template_filters.py
@@ -5,5 +5,4 @@ from dateutil.relativedelta import relativedelta
 
 def calculate_date_date(date_str, format_str):
     start_date = datetime.datetime.strptime(date_str, format_str)
-    print('date', start_date)
     return relativedelta(datetime.datetime.now(), start_date).years
